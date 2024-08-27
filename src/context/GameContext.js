eb45e73b8621a79c537bf67d5b3e53c450b0f2f9
David Morton <101235797+grumble-down@users.noreply.github.com>
2024-08-27 11:44:24 -0400

    Update GameContext.js
    Added additional phases

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -7,7 +7,7 @@ export const GameContext = createContext();
 export const GameProvider = ({ children }) => {
   const [currentPhase, setCurrentPhase] = useState("Combat Phase");
   const [currentTurn, setCurrentTurn] = useState("Player");
-  const phases = ["Combat Phase", "Movement Phase", "Shooting Phase"]; // Add all phases of the game as needed
+  const phases = ["Combat Phase", "Hero Phase", "Movement Phase","Shooting Phase", "Charge Phase", "Fight Phase", "End Phase"]; // Add all phases of the game as needed
 
   return (
     <GameContext.Provider
