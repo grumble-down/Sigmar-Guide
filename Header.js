29c85822239382049a9ed71f1bdd0193bbed0ba2
David Morton <101235797+grumble-down@users.noreply.github.com>
2024-08-27 00:45:04 -0400

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,18 +1,18 @@
 // Header.js
-// import React from "react";
-// import "./Header.css"; // Optional: Include a CSS file specifically for the header styling
+import React from "react";
+import "./Header.css"; // Optional: Include a CSS file specifically for the header styling
 
-// const Header = () => {
-  // return (
-    // <header className="app-header">
-      // <h1>Tabletop Companion App</h1>
-      // <nav className="app-nav">
-        // {/* Future navigation links can be added here */}
-        // {/* <a href="#home">Home</a> */}
-        // {/* <a href="#settings">Settings</a> */}
-      // </nav>
-   // </header>
-  // );
-// };
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h1>Tabletop Companion App</h1>
+      <nav className="app-nav">
+        {/* Future navigation links can be added here */}
+        {/* <a href="#home">Home</a> */}
+        {/* <a href="#settings">Settings</a> */}
+      </nav>
+    </header>
+  );
+};
 
-// export default Header;
+export default Header;
